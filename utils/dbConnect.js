0d57b4aa35f32a75cac65ee70a5174a73cf566c5
7starswin <zz4rpnsgry@privaterelay.appleid.com>
2026-03-11 22:30:32 +0700

    Update dbConnect.js

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,19 +1,12 @@
 const mongoose = require("mongoose")
 
-async function connectDB(){
-
-try{
-
-await mongoose.connect(process.env.MONGO_URI)
-
-console.log("MongoDB Connected")
-
-}catch(err){
-
-console.log(err)
-
-}
-
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("✅ MongoDB connected")
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 module.exports = connectDB

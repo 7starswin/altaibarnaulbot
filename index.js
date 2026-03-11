32060267c12bcd9c88caf69958b04de069a00e1d
7starswin <zz4rpnsgry@privaterelay.appleid.com>
2026-03-11 22:29:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require("dotenv").config()
 
-const { Telegraf } = require("telegraf")
 const connectDB = require("./utils/dbConnect")
-
 const bot = require("./bot")
 
-connectDB()
+async function start() {
+  await connectDB()
+  await bot.launch()
+  console.log("🚀 Bot started")
+}
 
-bot.launch()
-
-console.log("🚀 Bot Started")
+start()
 
 process.once("SIGINT", () => bot.stop("SIGINT"))
 process.once("SIGTERM", () => bot.stop("SIGTERM"))
